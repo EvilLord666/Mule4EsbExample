eaa59dbdea2d373cf2ffe71a0603783959761474
Michael Ushakov <um.nix.user@gmail.com>
2018-07-27 14:43:47 +0300

    now it is running

--- src/main/java/ru/abcconsulting/wfmplatform/ReportsManager.java
+++ src/main/java/ru/abcconsulting/wfmplatform/ReportsManager.java
@@ -2,12 +2,23 @@ package ru.abcconsulting.wfmplatform;
 
 import java.util.*;
 
+///
+/// See https://docs.mulesoft.com/mule4-user-guide/v/4.1/intro-java-integration
+/// and
+/// https://docs.mulesoft.com/connectors/java-invoke-method
+/// for how to invoke Java code
+///
 public class ReportsManager {
      public ReportsManager() {
     	 
      }
      
-     public void generateReport(Object[] reportParameters) {
+     ///
+     /// data are going through transform message component:
+     /// https://docs.mulesoft.com/mule4-user-guide/v/4.1/transform-component-about
+     /// messageBody is string representation of HTTP Request
+     ///
+     public void generateReport(String messageBody) {
     	 try {
     	     showStateImpl("Started");
     	     // todo: umv: generate impl
